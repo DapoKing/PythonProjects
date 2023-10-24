069e1adbb0d110bf23a6e53740d999ef3f66ac1a
Dapo King <138389040+DapoKing@users.noreply.github.com>
2023-10-24 14:33:22 +0100

    Update Euler_Projects
    Thoughts and ideas of how to solve

---
+++ Euler_Projects/solution0002.py
@@ -0,0 +1,13 @@
+#my first tboughts are to create an algorithm for the Fibonacci sequence, if there isn't already
+
+#using indexes to change and append lists in a while loop.
+
+#the question is to sum the even valued terms. 
+
+#instead I can just sum the 4th term in an index
+
+#the first even number is in position [1] and the next [5] 
+
+# So I'd just need to keep adding those numbers using sum() to a list that's largest number is less than 4 million
+
+# that less than 4 million part could be included in the while loop
