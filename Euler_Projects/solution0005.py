5eeb393bf3fd8de2a4e67adbb3e611115c9ceaa1
DapoKing <dapo_king@hotmail.com>
2023-10-27 17:11:31 +0100

    done :)

--- Euler_Projects/solution0005.py
+++ Euler_Projects/solution0005.py
@@ -2,19 +2,19 @@
 # are we using primes? like why not.
 # primes below 20...
 
-print(2*3*4*5*6*7*8*9*10)
-print(2*3*5*7)
-print(8*9*5)
+#print(2*3*4*5*6*7*8*9*10)
+#print(2*3*5*7)
+#print(8*9*5)
 
 #this is a question of least common multiple.
 #and from memory you remove duplicates of prime factors, the most number of those primes that's found in that number
 
-print(6*7*8*9*10)
+#print(6*7*8*9*10)
 
-print(8*5*9*7) #finally lol
+#print(8*5*9*7) #finally lol
 #so for numbers up to 20
 
-print(16*9*5*7*11*13*17*19)
+#print(16*9*5*7*11*13*17*19)
 
 #now how to write that in code.
 #I swear there has to be a LCM function that exists lmao
@@ -22,4 +22,5 @@ print(16*9*5*7*11*13*17*19)
 import math
 print(math.lcm(4,5,6,9,15,18))
 
-print(math.lcm(int(list(range(1,21)))))
+numbers = list(range(1,21))
+print(math.lcm(*numbers))
